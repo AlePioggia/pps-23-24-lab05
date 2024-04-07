a63f80808376040590c3fdc7e502018476397fef
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-04-07 21:59:12 +0200

    add task 3 template

---
+++ src/main/scala/ex/minesweeper/Logics.java
@@ -0,0 +1,40 @@
+package ex.minesweeper;
+
+public interface Logics {
+
+    /**
+     * attempt to move Knight on position row,col, if possible
+     * 
+     * @param row
+     * @param col
+     * @return whether the pawn has been hit
+     */
+    boolean hit(int row, int col);
+
+    /**
+     * @param row
+     * @param col
+     * @return whether position row,col has the knight
+     */
+    boolean hasKnight(int row, int col);
+
+    /**
+     * @param row
+     * @param col
+     * @return whether position row,col has the pawn
+     */
+    boolean hasPawn(int row, int col);
+
+    /**
+     * 
+     * @return pawn's position
+     */
+    Pair<Integer, Integer> getPawn();
+
+    /**
+     * 
+     * @return Knight's position
+     */
+    Pair<Integer, Integer> getKnight();
+
+}
---
+++ src/main/scala/ex/minesweeper/LogicsImpl.java
@@ -0,0 +1,41 @@
+package ex.minesweeper;
+
+import java.util.*;
+
+public class LogicsImpl implements Logics {
+
+	public LogicsImpl(int size) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'LogicsImpl'");
+	}
+
+	@Override
+	public boolean hit(int row, int col) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+	}
+
+	@Override
+	public boolean hasKnight(int row, int col) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'hasKnight'");
+	}
+
+	@Override
+	public boolean hasPawn(int row, int col) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'hasPawn'");
+	}
+
+	@Override
+	public Pair<Integer, Integer> getPawn() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'getPawn'");
+	}
+
+	@Override
+	public Pair<Integer, Integer> getKnight() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'getKnight'");
+	}
+}
