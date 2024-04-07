bfc2a07e764de13df715f36d26a88c978f9e5ea4
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-04-07 21:34:12 +0200

    complete task2 (optimization needed)

--- src/main/java/polyglot/a05b/GUI.java
+++ src/main/java/polyglot/a05b/GUI.java
@@ -9,18 +9,18 @@ import polyglot.Pair;
 public class GUI extends JFrame {
 
     private static final long serialVersionUID = -6218820567019985015L;
-    private final Map<JButton, Pair<Integer,Integer>> cells = new HashMap<>();
+    private final Map<JButton, Pair<Integer, Integer>> cells = new HashMap<>();
     private final JButton moveButton = new JButton(">");
     private final Logics logics;
 
     public GUI(int size) {
         this.logics = new LogicsImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(100*size, 100*size);
+        this.setSize(100 * size, 100 * size);
 
-        JPanel panel = new JPanel(new GridLayout(size,size));
-        this.getContentPane().add(BorderLayout.CENTER,panel);
-        this.getContentPane().add(BorderLayout.SOUTH,moveButton);
+        JPanel panel = new JPanel(new GridLayout(size, size));
+        this.getContentPane().add(BorderLayout.CENTER, panel);
+        this.getContentPane().add(BorderLayout.SOUTH, moveButton);
 
         moveButton.addActionListener(e -> {
             logics.tick();
@@ -30,10 +30,10 @@ public class GUI extends JFrame {
             }
         });
 
-        for (int i=0; i<size; i++){
-            for (int j=0; j<size; j++){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 final JButton jb = new JButton(" ");
-                this.cells.put(jb,new Pair<>(j,i));
+                this.cells.put(jb, new Pair<>(j, i));
                 panel.add(jb);
             }
         }
@@ -42,7 +42,7 @@ public class GUI extends JFrame {
     }
 
     private void updateView() {
-        cells.forEach((b,p)-> b.setText( logics.hasElement(p.getX(), p.getY()) ? "*" : " "));
+        cells.forEach((b, p) -> b.setText(logics.hasElement(p.getX(), p.getY()) ? "*" : " "));
     }
 
 }
