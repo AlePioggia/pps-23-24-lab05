1f714434ecfb1385033c468cda5f6ac7902bf718
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2024-04-08 15:10:26 +0200

    clean code

--- src/main/scala/ex/minesweeper/Logics.scala
+++ src/main/scala/ex/minesweeper/Logics.scala
@@ -10,36 +10,32 @@ trait Logics:
     def hasPawn(row: Int, col: Int): Boolean
 
 object Logics:
-    def apply(
-        pawn: Pair[Int, Int],
-        knight: Pair[Int, Int],
-        size: Int
-    ): Logics = 
-        LogicsImpl(pawn, knight, size)
+    def apply(pawn: Pair[Int, Int], knight: Pair[Int, Int], size: Int): Logics = LogicsImpl(pawn, knight, size)
 
-    private case class LogicsImpl(
-        override val pawn: Pair[Int, Int],
-        override val knight: Pair[Int, Int],
-        val size: Int
-    ) extends Logics:
-
-      private val pawnPiece: ChessPiece = Pawn(PiecesGenerator(size).generatePawnInArandomPosition());
-      private val knightPiece: ChessPiece = Knight(PiecesGenerator(size).generateKnightInARandomPosition())
+    private case class LogicsImpl(override val pawn: Pair[Int, Int], override val knight: Pair[Int, Int], val size: Int) extends Logics:
+      private val pawnPiece: ChessPiece = ChessPiece(PiecesGenerator(size).randomPosition(), PieceName.Pawn)
+      private val knightPiece: ChessPiece = ChessPiece(PiecesGenerator(size).randomPosition(), PieceName.Knight)
 
       override def hasPawn(row: Int, col: Int): Boolean = pawnPiece.position == Pair(row, col)
-
       override def hasKnight(row: Int, col: Int): Boolean = knightPiece.position == Pair(row, col)
+      override def hit(row: Int, col: Int): Boolean = if knightPiece.move(row, col) then pawnPiece.position == knightPiece.position else false
 
-      override def hit(row: Int, col: Int): Boolean = 
-        if knightPiece.move(row, col) then pawnPiece.position == knightPiece.position else false
+enum PieceName:
+  case Pawn, Knight
 
 trait ChessPiece:
     def position: Pair[Int, Int]
     def position_(row: Int, col: Int): Unit = Pair(row, col)
     def move(row: Int, col: Int): Boolean
 
-object Knight:
-    def apply(position: Pair[Int, Int]): ChessPiece = KnightImpl(position)
+object ChessPiece:
+    def apply(position: Pair[Int, Int], pieceName: PieceName): ChessPiece = pieceName match {case PieceName.Pawn => PawnImpl(position) case PieceName.Knight => KnightImpl(position)}
+    
+    private case class PawnImpl(var position: Pair[Int, Int]) extends ChessPiece:
+        override def move(row: Int, col: Int): Boolean = 
+            position = Pair(row, col)
+            position == Pair(row, col)
+
     private case class KnightImpl(var position: Pair[Int, Int]) extends ChessPiece:
         override def move(row: Int, col: Int): Boolean =
             val x: Int = row - position.getX
@@ -50,27 +46,16 @@ object Knight:
                 true 
             else false
 
-object Pawn:
-    def apply(position: Pair[Int, Int]): ChessPiece = PawnImpl(position)
-    private case class PawnImpl(var position: Pair[Int, Int]) extends ChessPiece:
-        override def move(row: Int, col: Int): Boolean = 
-            position = Pair(row, col)
-            position == Pair(row, col)
-
 trait PiecesGenerator:
-    def generatePawnInArandomPosition(): Pair[Int, Int]
-    def generateKnightInARandomPosition(): Pair[Int, Int]
+    def randomPosition(): Pair[Int, Int]
 
 object PiecesGenerator:
     def apply(size: Int): PiecesGenerator = PiecesGeneratorImpl(size)
-    private case class PiecesGeneratorImpl(size: Int) extends PiecesGenerator:
+    
+    private case class PiecesGeneratorImpl(size: Int, val pawnPosition: Pair[Int, Int] = Pair(0, 0), val knightPosition: Pair[Int, Int] = Pair(0, 0))
+     extends PiecesGenerator:
         val random: Random = Random()
-        val pawnPosition: Pair[Int, Int] = randomPosition
-        val knightPosition: Pair[Int, Int] = randomPosition
 
-        override def generatePawnInArandomPosition(): Pair[Int, Int] = randomPosition
-        override def generateKnightInARandomPosition(): Pair[Int, Int] = randomPosition
-
-        def randomPosition: Pair[Int, Int] = 
+        override def randomPosition(): Pair[Int, Int] = 
             val pos: Pair[Int, Int] = Pair(random.nextInt(size), random.nextInt(size))
-            if (pawnPosition != null && pawnPosition == pos || knightPosition != null && knightPosition == randomPosition) then randomPosition else pos 
+            if (pawnPosition == pos || knightPosition == randomPosition()) then randomPosition() else pos 
